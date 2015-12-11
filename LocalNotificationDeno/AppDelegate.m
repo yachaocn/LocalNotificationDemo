86185925a1c6da84367a40e1a234aea15f1a51c0
yachaocn <zyachao@163.com>
2015-12-11 12:39:08 +0800

    完成本地通知

--- LocalNotificationDeno/AppDelegate.m
+++ LocalNotificationDeno/AppDelegate.m
@@ -10,36 +10,80 @@
 
 @interface AppDelegate ()
 
+@property(nonatomic,strong) NSDateComponents *dateComponets;
+@property(nonatomic,strong) UILocalNotification *localNoti;
+@property(nonatomic) NSInteger i;
 @end
 
 @implementation AppDelegate
 
+-(void)addMinute:(NSTimer *)sender
+{
+    self.localNoti.fireDate = [self.localNoti.fireDate dateByAddingTimeInterval:100];
+    self.localNoti.timeZone = [NSTimeZone defaultTimeZone];
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:self.localNoti];
+    NSLog(@"%@",self.localNoti.fireDate);
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    CGFloat systemVersion = [[UIDevice currentDevice].systemVersion floatValue];
+    if (systemVersion >= 8.0) {
+//        申请用户权限
+        UIUserNotificationType types = UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound;
+        UIUserNotificationSettings *setting = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication]registerUserNotificationSettings:setting];
+    }
+//    创建本地通知
+    UILocalNotification *localNoti = [[UILocalNotification alloc]init];
+    self.localNoti = localNoti;
+    if (nil != localNoti) {
+        NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
+        NSDateComponents *dateComponets = [[NSDateComponents alloc]init];
+        
+        dateComponets.year = 2015;
+        dateComponets.month = 12;
+        dateComponets.day = 11;
+        dateComponets.hour = 12;
+        dateComponets.minute = 30;
+        self.dateComponets = dateComponets;
+        NSDate *date = [calendar dateFromComponents:dateComponets];
+        
+      NSTimer *timer = [NSTimer timerWithTimeInterval:3*60 target:self selector:@selector(addMinute:) userInfo:nil repeats:YES];
+        [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+        
+        localNoti.fireDate = date;
+        localNoti.timeZone = [NSTimeZone defaultTimeZone];
+        localNoti.alertTitle = @"提示！";
+        localNoti.alertBody = @"该吃饭了！";
+        localNoti.alertAction = @"查看！";
+        localNoti.soundName = UILocalNotificationDefaultSoundName;
+        localNoti.applicationIconBadgeNumber = 1;
+        NSDictionary *userinfo = [NSDictionary dictionaryWithObject:@"你好！" forKey:@"nihao"];
+        localNoti.userInfo = userinfo;
+//        启动本地通知
+        [[UIApplication sharedApplication] scheduleLocalNotification:localNoti];
+        
+        //收到通知
+        UILocalNotification *not = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
+        if (not) {
+            NSDictionary *dic = not.userInfo;
+            NSString *str = dic[@"nihao"];
+            NSLog(@"%@",str);
+        }
+    }
     return YES;
 }
+-(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+        //收到通知
+        if (notification) {
+            NSDictionary *dic = notification.userInfo;
+            NSString *str = dic[@"nihao"];
+            NSLog(@"%@",str);
+        }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
